092a42eab0ec80db7e54a21ece64d87722629a12
İsa <isatoltar@gmail.com>
2019-10-14 14:50:28 +0300

    bootstrap added

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,10 @@
 $('#btn').click(() => {
-  $('#box').toggle();
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/posts/10',
+    beforeSend: function(xhr) {
+      xhr.overrideMimeType('text/plain; charset=x-user-defined');
+    },
+  }).done(function(data) {
+    $('#data').text(data);
+  });
 });
