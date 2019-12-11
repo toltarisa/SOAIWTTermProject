37caffc26461466895b3d830a6401948293db415
Engincan Veske <enginveske@gmail.com>
2019-12-11 14:27:38 +0300

    london maps controller has been added

---
+++ SmartLondon/SmartLondon/Controllers/LondonMapsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartLondon.Controllers
+{
+    public class LondonMapsController : Controller
+    {
+        public IActionResult Index(string searchKey)
+        {
+            if (string.IsNullOrWhiteSpace(searchKey))
+                return View();
+            else
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
